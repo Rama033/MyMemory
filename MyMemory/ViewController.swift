a3cb89caa0b1eac3a6705351d7ef6e10c25b5273
CE-MAC-23 <ce-mac-23@ce-mac-23ui-macbook-pro.local>
2018-07-26 14:14:46 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by CE-MAC-23 on 2018. 7. 26..
+//  Copyright © 2018년 CE-MAC-23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
